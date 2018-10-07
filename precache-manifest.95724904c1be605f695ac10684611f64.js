6c91ce0f0bb4747188904fea4ec477523cbc30a0
kar-re <kaspianjakobsson@gmail.com>
2018-10-07 19:44:06 +0200

    Updates

---
+++ precache-manifest.95724904c1be605f695ac10684611f64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8a9d2da0bec0026972c",
+    "url": "/ReactXoX/static/js/runtime~main.d8a9d2da.js"
+  },
+  {
+    "revision": "4a5d3c31b6d9490662a3",
+    "url": "/ReactXoX/static/js/main.4a5d3c31.chunk.js"
+  },
+  {
+    "revision": "7e802ebce41dd84e0e5e",
+    "url": "/ReactXoX/static/js/1.7e802ebc.chunk.js"
+  },
+  {
+    "revision": "4a5d3c31b6d9490662a3",
+    "url": "/ReactXoX/static/css/main.60704d2d.chunk.css"
+  },
+  {
+    "revision": "1d28167d305067a0442ad422f4ebf909",
+    "url": "/ReactXoX/index.html"
+  }
+];
